8b4f09add3450adc82142e590a2da17933f5d0a2
Aluno São Lucas <aluno.saolucas@ensino.ulbra.net>
2025-11-25 22:36:39 -0300

    front do login e register.

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<style>
+body{
+    background-color: #f8f8ec;
+    margin: 0;
+}
+
+
+</style>
+
+<body>
+
+<?php
+    include 'templates/cabecalho.php';
+    ?>
+
+
+
+ 
+
+    
+</body>
+</html>
