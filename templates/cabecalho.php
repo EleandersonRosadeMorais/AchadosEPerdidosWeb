e1a3d454f29a6f370fab8c8571e91b0b0cd21757
Aluno São Lucas <aluno.saolucas@ensino.ulbra.net>
2025-11-26 22:32:48 -0300

    tela administrador e css

--- templates/cabecalho.php
+++ templates/cabecalho.php
@@ -1,50 +1,5 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-    <meta charset="UTF-8">
-    <title>Header</title>
-
-    <style>
-        header {
-            overflow: hidden;
-            background-color: #02416D;
-            padding: 20px 10px;
-            color: #ffffff;
-            text-align: center;
-
-        }
-
-        header a {
-            color: black;
-            padding: 12px;
-            text-decoration: none;
-            font-size: 18px;
-            border-radius: 4px;
-            color: #ffffff;
-            font-size: 20px;
-            font-weight: bold;
-        }
-
-        nav {
-            display: flex;
-            gap: 10px;
-            justify-content: center;
-        }
-
-        header a:hover {
-            background-color: #ddd;
-            color: black;
-        }
-
-      
-    </style>
-
-</head>
-
-<body>
-    <header>
-        <h1>Bem-vindo ao Meu Site</h1>
+ <header>
+        <h1 id="titulo">Bem-vindo ao Meu Site</h1>
         <nav>
             <a href="index.php">Início</a>
             <a href="index.php">Gerenciamento</a>
@@ -52,4 +7,3 @@
             <a href="sobre.php">Sobre</a>
         </nav>
     </header>
-    <main>
